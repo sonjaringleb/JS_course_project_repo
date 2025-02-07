c5622c59abf1ff8b344c9f81fe2110a154f23e66
Sonja Ringleb <Sonja.Ringleb@gmail.com>
2025-02-06 19:21:59 -0500

    Product Survey: fixed error

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,18 +1,21 @@
-const username = document.getElementById('name').value;
-const age = document.getElementById('age').value;
-const email = document.getElementById('email').value;
-const job = document.getElementById('job').value;
-const designation = document.getElementById('designation').value;
-const productType = document.getElementById('productType').value;
-const feedback = document.getElementById('feedbackText').value;
+
 const submitButton = document.getElementById('submitBtn');
 
-submitButton.onclick = submitFeedback();
+submitButton.onclick = submitFeedback;
 
 function submitFeedback() {
+    const username = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const email = document.getElementById('email').value;
+    const job = document.getElementById('job').value;
+    const designation = document.getElementById('designation').value;
+    const productType = document.getElementById('productType').value;
+    const feedback = document.getElementById('feedbackText').value;
+
     alert('Thank you for your valuable feedback');
 
-    // put document.getElementById('userName').innerHTML = username;
+    // put user values into div
+    document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
     document.getElementById('userEmail').innerHTML = email;
     document.getElementById('userJob').innerHTML = job;
