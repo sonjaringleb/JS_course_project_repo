c1310ac75b123ac8c6d71b5e1690f55d2ac89ab5
Sonja Ringleb <Sonja.Ringleb@gmail.com>
2025-03-01 11:43:45 -0500

    bookSystem: Add deleteBook function

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -1,44 +1,55 @@
 let books = [];
+let bookID = books.length + 1;
+let deleteButton = document.getElementsByClassName("deleteButton");
+
+//deleteButton.addEventListener("click", () => {
+//  console.log("deleteButton ${index + 1} has been clicked.");
+//});
 
 function addBook() {
-    const bookName = document.getElementById('bookName').value;
-    const authorName = document.getElementById('authorName').value;
-    const bookDescription = document.getElementById('bookDescription').value;
-    const pagesNumber = parseInt(document.getElementById('pagesNumber').value);
-    if (bookName && authorName && bookDescription && !isNaN(pagesNumber)) {
-        const book = {
-            name: bookName,
-            authorName: authorName,
-            bookDescription: bookDescription,
-            pagesNumber: pagesNumber
-        };
-        books.push(book);
-        showbooks();
-        clearInputs();
-    } else {
-        alert('Please fill in all fields correctly.');
-    }
+  const bookName = document.getElementById("bookName").value;
+  const authorName = document.getElementById("authorName").value;
+  const bookDescription = document.getElementById("bookDescription").value;
+  const pagesNumber = parseInt(document.getElementById("pagesNumber").value);
+  if (bookName && authorName && bookDescription && !isNaN(pagesNumber)) {
+    const book = {
+      name: bookName,
+      authorName: authorName,
+      bookDescription: bookDescription,
+      pagesNumber: pagesNumber,
+    };
+    books.push(book);
+    showbooks();
+    clearInputs();
+  } else {
+    alert("Please fill in all fields correctly.");
+  }
 }
 
 function showbooks() {
-    const booksDiv = books.map((book, index) => `<h1>Book Number: ${index + 1}</h1>
+  const booksDiv = books.map(
+    (book, index) => `<h1>Book Number: ${index + 1}</h1>
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
         <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
-        <button onclick="deleteBook">Delete</button>`
-         // Add Delete Button
-    );
-    document.getElementById('books').innerHTML = booksDiv.join('');
+        <button class="deleteButton" id=${
+          index + 1
+        } onclick="deleteBook(${index})">Delete</button>`
+    // Add Delete Button
+  );
+
+  document.getElementById("books").innerHTML = booksDiv.join("");
 }
 
 function clearInputs() {
-    document.getElementById('bookName').value = '';
-    document.getElementById('authorName').value = '';
-    document.getElementById('bookDescription').value = '';
-    document.getElementById('pagesNumber').value = '';
+  document.getElementById("bookName").value = "";
+  document.getElementById("authorName").value = "";
+  document.getElementById("bookDescription").value = "";
+  document.getElementById("pagesNumber").value = "";
 }
 
-function deleteBook() {
-    // to do
+function deleteBook(index) {
+  books.splice(index, 1);
+  showbooks();
 }
--- groceryTracker/grocery_Tracker.js
+++ groceryTracker/grocery_Tracker.js
@@ -2,13 +2,25 @@ let grocery1;
 let grocery2;
 let grocery3;
 
-function calculateTotalAmount(){
-    grocery1 = parseFloat(document.getElementById('grocery1').value);
-    grocery2 = parseFloat(document.getElementById('grocery2').value);
-    grocery3 = parseFloat(document.getElementById('grocery3').value);
+function calculateTotalAmount() {
+  grocery1 = parseFloat(document.getElementById("grocery1").value);
+  grocery2 = parseFloat(document.getElementById("grocery2").value);
+  grocery3 = parseFloat(document.getElementById("grocery3").value);
 
-    let totalAmount = grocery1 + grocery2 + grocery3;
-    let totalAmountRounded = totalAmount.toFixed(2);
+  let totalAmount = grocery1 + grocery2 + grocery3;
+  let totalAmountRounded = totalAmount.toFixed(2);
 
-    document.getElementById('result').innerText = `The total amount of your groceries is: $ ${totalAmountRounded}`;
+  document.getElementById(
+    "result"
+  ).innerText = `The total amount of your groceries is: $ ${totalAmountRounded}`;
+}
+
+function addGroceryAmount() {
+  const inputs = document.getElementById("groceryAmounts").elements;
+  let inputNumber = inputs.length;
+
+  const newInput = document.createElement("input");
+
+  inputs.type = "number";
+  inputs.id = "grocery" + inputNumber + 1;
 }
