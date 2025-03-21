47a152930e34f37ec2d008c559e236a42095fec2
Sonja Ringleb <Sonja.Ringleb@gmail.com>
2025-03-21 12:05:57 -0400

    Weather Report: use other weather API and fix js code

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,18 +1,20 @@
 function showweatherDetails(event) {
-      event.preventDefault();
-       const lat = document.getElementById('lat').value;
-       const lon = document.getElementById('lon').value;
-      const apiKey = '266dc74b20b44aae56a1d94d8a94898d'; // Replace 'YOUR_API_KEY' with your actual API key
-      const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}}&appid={API key}`;
+    event.preventDefault();
+    const lat = document.getElementById("lat").value;
+    const lon = document.getElementById("lon").value;
+    const apiKey = "92b1e6384db2868d99fc908e6cc64d3e"; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m&current=temperature_2m`;
 
-       fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-          const weatherInfo = document.getElementById('weatherInfo');
-          weatherInfo.innerHTML = `<h2>Weather in ${data.weather}</h2>
-                                  <p>Temperature: ${data.temp} &#8451;</p>
-                                  <p>Weather: ${data.weather[0].description}</p>`;
-        })
-    }
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            const weatherInfo = document.getElementById("weatherInfo");
+            weatherInfo.innerHTML = `<h2>Weather in latitude: ${data.latitude}, longitude: ${data.longitude}</h2>
+                                    <p>Temperature: ${data.current.temperature_2m} &#8451;</p>`;
+        });
+}
 
- document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+document
+    .getElementById("weatherForm")
+    .addEventListener("submit", showweatherDetails);
